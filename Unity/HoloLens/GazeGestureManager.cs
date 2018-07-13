67a649bbcce3b5ab11b14bb2f2513621983717e4
zhangbo_fh <bo_zhang1993@163.com>
2018-07-13 16:57:57 +0800

    Add HoloLensCode

---
+++ Unity/HoloLens/GazeGestureManager.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.WSA.Input;
+
+public class GazeGestureManager : MonoBehaviour {
+
+    public static GazeGestureManager Instance { get; private set; }
+    public GameObject FocusedObject { get; private set; }
+    public GameObject MainPanel;
+
+    GestureRecognizer gestureRecognizer;
+
+    void Awake() {
+        Instance = this;
+        //实例化 手势识别对象
+        gestureRecognizer = new GestureRecognizer();
+        //指定手势识别类型
+        gestureRecognizer.SetRecognizableGestures(GestureSettings.Tap | GestureSettings.Hold );
+        //注册手势事件
+        gestureRecognizer.Tapped += Recognizer_TappedEvent;
+        gestureRecognizer.HoldStarted += Recognizer_HoldStartedEvent;
+        gestureRecognizer.HoldCompleted += Recognizer_HoldCompletedEvent;
+        gestureRecognizer.StartCapturingGestures();
+    }
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject oldFocusObject = FocusedObject;
+
+        Vector3 headPosition = Camera.main.transform.position;
+        Vector3 gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            FocusedObject = hitInfo.collider.gameObject;
+        }
+        else {
+            FocusedObject = null;
+        }
+
+        if (FocusedObject != oldFocusObject) {
+            gestureRecognizer.CancelGestures();
+            gestureRecognizer.StartCapturingGestures();
+        }
+		
+	}
+
+    void Recognizer_TappedEvent(TappedEventArgs args) {
+        //Debug.Log("Tap");
+        OnSelectedOption("OnTapSelected");
+    }
+
+    void Recognizer_HoldStartedEvent(HoldStartedEventArgs args) {
+        //Debug.Log("Hold");    
+        OnSelectedOption("OnHoldSelected");
+    }
+
+    void Recognizer_HoldCompletedEvent(HoldCompletedEventArgs args) {
+       //Debug.Log("HoldCompleted");
+        OnSelectedOption("OnHoldCompleted");
+    }
+
+    void OnSelectedOption(string functionName) {
+        //如果此时凝视对象不为空，执行凝视对象的自身响应
+        if (FocusedObject != null) {
+            //Debug.Log("FocusedObjected:" + FocusedObject.name);
+            FocusedObject.SendMessageUpwards(functionName,SendMessageOptions.DontRequireReceiver);
+        }
+
+        //否则如果没有主UI面板没有显示，则显示主UI面板
+        else{
+            if (MainPanel.activeSelf != true) {
+                if (SceneManager.GetActiveScene().name == "IDC_MachineRoom") {
+                    MainPanel.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 1f;
+                }
+                MainPanel.SetActive(true);
+            }    
+        }
+    }
+
+    void OnDestroy()
+    {
+        gestureRecognizer.Tapped -= Recognizer_TappedEvent;
+        gestureRecognizer.HoldStarted -= Recognizer_HoldStartedEvent;
+        gestureRecognizer.HoldCompleted -= Recognizer_HoldCompletedEvent;
+    }
+}
---
+++ Unity/HoloLens/WorldCursor.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 管理场景中凝视UI，当凝视某物体时，使用一种UI，当没有凝视物体时，使用另一种UI
+/// </summary>
+public class WorldCursor : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject CursorOn;
+    [SerializeField]
+    private GameObject CursorOff;
+    [SerializeField]
+    private float cursorDistance = 2.0f;
+    [SerializeField]
+    private float scaleAmount = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+        CursorOn.SetActive(false);
+        CursorOff.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 headPosition = Camera.main.transform.position;
+        Vector3 gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            CursorOff.SetActive(false);
+            //设置凝视选中物体时UI的位置和旋转，使其在合适位置并始终朝向相机
+            CursorOn.transform.position = hitInfo.point-gazeDirection*0.1f;
+            Quaternion targetRotation = Quaternion.FromToRotation(CursorOn.transform.up, headPosition - transform.position)* CursorOn.transform.rotation;
+            CursorOn.transform.rotation = Quaternion.Lerp(CursorOn.transform.rotation,targetRotation,2.0f*Time.deltaTime);
+            ScaleWithTan(CursorOn.transform);
+            CursorOn.SetActive(true);          
+        }
+        else {
+            CursorOn.SetActive(false);
+            //设置凝视没有选中物体时UI的位置和旋转，使其在合适位置并始终朝向相机
+            CursorOff.transform.position = headPosition + gazeDirection*cursorDistance;
+            Quaternion targetRotation = Quaternion.FromToRotation(CursorOff.transform.up, headPosition - transform.position)* CursorOff.transform.rotation;
+            CursorOff.transform.rotation= Quaternion.Lerp(CursorOff.transform.rotation, targetRotation, 2.0F*Time.deltaTime);
+            ScaleWithTan(CursorOff.transform);
+            CursorOff.SetActive(true);
+        }
+	}
+
+    //根据Cursor距离屏幕的远近缩放其scale
+    void ScaleWithTan(Transform target) {
+        float tan = Mathf.Tan((Camera.main.fieldOfView/180.0f)*Mathf.PI);
+        float distance = Vector3.Distance(target.transform.position,Camera.main.transform.position);
+        float deltaScale = tan * distance;
+        target.transform.localScale = new Vector3(deltaScale,deltaScale,deltaScale)*scaleAmount;
+        //Debug.Log(target.transform.localScale);
+    }
+}
